a14935ff13c60cda59c3df2b1de2ce7305eda84c
Erik Nagel <erik@reallyhuman.net>
2021-07-19 12:06:56 +0200

    FileChecker BugFix bei Uebergabe von Dateinamen ohne Pfadangabe.

--- FileCheckerDemo/Program.cs
+++ FileCheckerDemo/Program.cs
@@ -29,7 +29,9 @@ namespace FileCheckerDemo
                         }
                     }
                     Console.WriteLine();
-                    res = FileChecker.Run(@"TRACE|TestFiles\.*\..+|<|S:15|Prüft, dass Test-Files nicht älter als 15 Sekunden werden.", new TreeParameters("MainTree", null), null);
+                    //res = FileChecker.Run(@"COUNT|TestFiles\.*\.+|<|4|Prüft, ob mehr als drei Testfiles vorhanden sind.", new TreeParameters("MainTree", null), null);
+                    res = FileChecker.Run(@"COUNT|.*\.*|<|4|Prüft, ob mehr als drei Testfiles vorhanden sind.", new TreeParameters("MainTree", null), null);
+                    //res = FileChecker.Run(@"TRACE|TestFiles\.*\..+|<|S:15|Prüft, dass Test-Files nicht älter als 15 Sekunden werden.", new TreeParameters("MainTree", null), null);
                     //res = FileChecker.Run(@"AGE|\\localhost\Daten\Test.csv|<!|d:3|Prüft, ob die Test.csv älter als 3 Tage ist.", new TreeParameters("MainTree", null), null);
                     //res = FileChecker.Run(@"COUNT|\\localhost\Daten\<Regex>\.pdf|>|150|Prüft, ob mindestens 150 PDFs erzeugt wurden.", new TreeParameters("MainTree", null), null);
                     Console.WriteLine("Ergebnis: {0}", (FileChecker.ReturnObject as FileCheckerReturnObject).ToString());
