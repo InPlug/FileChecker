811bb90e04e131c30544d9f65ceb683261788eab
Erik Nagel <erik@reallyhuman.net>
2023-01-30 16:51:11 +0100

    *proj-Dateien: Zugriff auf Environment (Vishnu_Root, Assembly_Root) in AddOn/proj/* ausgelagert; Environment-Variablen sind jetzt optional.

--- FileCheckerDemo/Program.cs
+++ FileCheckerDemo/Program.cs
@@ -29,11 +29,11 @@ namespace FileCheckerDemo
                         }
                     }
                     Console.WriteLine();
-                    //res = FileChecker.Run(@"COUNT|TestFiles\.*\.+|<|4|Prüft, ob mehr als drei Testfiles vorhanden sind.", new TreeParameters("MainTree", null), null);
-                    res = FileChecker.Run(@"COUNT|.*\.*|<|4|Prüft, ob mehr als drei Testfiles vorhanden sind.", new TreeParameters("MainTree", null), null);
-                    //res = FileChecker.Run(@"TRACE|TestFiles\.*\..+|<|S:15|Prüft, dass Test-Files nicht älter als 15 Sekunden werden.", new TreeParameters("MainTree", null), null);
-                    //res = FileChecker.Run(@"AGE|\\localhost\Daten\Test.csv|<!|d:3|Prüft, ob die Test.csv älter als 3 Tage ist.", new TreeParameters("MainTree", null), null);
-                    //res = FileChecker.Run(@"COUNT|\\localhost\Daten\<Regex>\.pdf|>|150|Prüft, ob mindestens 150 PDFs erzeugt wurden.", new TreeParameters("MainTree", null), null);
+                    //res = FileChecker.Run(@"SIZE|TestFiles\FileCheckerDemo.exe.config|>|162|Prüft, ob die FileCheckerDemo.exe.config > 162 byte ist.", new TreeParameters("MainTree", null), null);
+                    //res = FileChecker.Run(@"COUNT|TestFiles\.*\..+|<|4|Prüft, ob mehr als drei Testfiles vorhanden sind.", new TreeParameters("MainTree", null), null);
+                    res = FileChecker.Run(@"TRACE|TestFiles\.*\..+|<|S:15|Prüft, dass Test-Files nicht älter als 15 Sekunden werden.", new TreeParameters("MainTree", null), null);
+                    //res = FileChecker.Run(@"AGE|TestFiles\FileCheckerDemo.exe.config|<!|d:3|Prüft, ob die FileCheckerDemo.exe.config älter als 3 Tage ist.", new TreeParameters("MainTree", null), null);
+                    Console.WriteLine("---------------------------------------------------------------------------------");
                     Console.WriteLine("Ergebnis: {0}", (FileChecker.ReturnObject as FileCheckerReturnObject).ToString());
                     Thread.Sleep(5000);
                 }
